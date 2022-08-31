e699dc1f7f23f7d80cd7ade486ce7e90a6ea4f3b
gaunt0066 <gaunt0066@gmail.com>
2022-08-31 10:39:51 +0300

    soldier

--- soldierboy.js
+++ soldierboy.js
@@ -3,7 +3,23 @@ const soldier = {
   health: 10,
   weapon: { nameWeapon: "m4a1", ammo: 30 },
   clip: 3,
-  shoot() {},
-  reload() {},
-  wounded() {},
+  shoot() {
+    if (soldier.weapon.ammo > 0) {
+      soldier.weapon.ammo--;
+      console.log("bang-bang");
+    } else {
+      console.log("Обойма пуста. Перезарядитесь");
+    }
+  },
+
+  reload() {
+    soldier.weapon.ammo=30
+    clip--
+  },
+  wounded() {
+soldier.health--
+if(soldier.health<=0){
+    console.log('Ты проиграл')
+}
+  },
 };
